401cf76e1e87978cba638af748f55fb80dfb2ae8
Aurelien Joncour <aurelien.joncour@epitech.eu>
2021-03-15 18:47:51 +0100

    chore(includes/lib/SFML.hpp) adding header

---
+++ includes/lib/SFML.hpp
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2021
+** B-OOP-400-REN-4-1-arcade-aurelien.joncour
+** File description:
+** SFML
+*/
+
+#ifndef SFML_HPP_
+#define SFML_HPP_
+
+#include "../IDisplayModule.hpp"
+
+class SFML : public arcade::IDisplayModule {
+    public:
+        ~SFML();
+
+    protected:
+    private:
+};
+
+#endif /* !SFML_HPP_ */
