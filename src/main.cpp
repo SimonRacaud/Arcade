01ad08bc9fa0aaedc8e714a6c964a18fc2065d59
RACAUD Simon <simon.racaud@epitech.eu>
2021-04-09 14:38:01 +0200

    chore(usage): update usage message

--- src/main.cpp
+++ src/main.cpp
@@ -6,26 +6,26 @@
 */
 
 #include <iostream>
-#include "Core/Core.hpp"
 #include "./exception/BaseException.hpp"
+#include "Core/Core.hpp"
 
 int main(int argc, char **argv)
 {
-    if (argc == 2) {
+    if (argc != 2 || std::string(argv[1]) == "-h") {
+        std::cout << "USAGE:\n\t" << argv[0]
+                  << " lib/[graphical-library-path].so" << std::endl;
+    } else {
         arcade::Core core(argv[1]);
         arcade::CoreConfig &config = core.getConfig();
         try {
             core.loop();
-        } catch (BaseException const& e) {
+        } catch (BaseException const &e) {
             std::cerr << e.what() << std::endl;
             return EXIT_ERROR;
         }
         if (config.getStatus() == arcade::CoreConfig::ExitStatus::ERROR) {
             return EXIT_ERROR;
         }
-    } else {
-        std::cout << "USAGE: " << argv[0] << " {{Graphical/library/path}}"
-                  << std::endl;
     }
     return EXIT_SUCCESS;
 }
